8e459735be9e2772a45b643d6477eaf51b81df3e
Laislon <0000113443649xsp@al.educacao.sp.gov.br>
2024-08-06 17:13:02 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(500, 500);
+  }
+  
+  function draw() {
+    background("#7C0E58");
+    strokeWeight("10")
+    fill("#E015C3")
+    
+    rect(mouseX,mouseY,160)
+  }
